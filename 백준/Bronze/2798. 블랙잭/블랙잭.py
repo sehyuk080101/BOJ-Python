071f96bb9bf5026de20dac5eebe29ef9b6c0b445
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2024-05-24 15:57:17 +0900

    [Bronze II] Title: 블랙잭, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,9 +1,9 @@
 N, M = map(int, input().split())
 m = 0
-a = set(map(int, input().split()))
-for i in a:
-    for j in a - {i}:
-        for k in a - {i, j}:
-            if m < i + j + k <= M:
-                m = i + j + k
+a = list(map(int, input().split()))
+for i in range(N - 2):
+    for j in range(i + 1, N - 1):
+        for k in range(j + 1, N):
+            if m < a[i] + a[j] + a[k] <= M:
+                m = a[i] + a[j] + a[k]
 print(m)
