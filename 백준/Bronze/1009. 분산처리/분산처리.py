5667ec816dbeadcf0f370d730051799fcb1dd1ed
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2024-06-03 20:51:20 +0900

    [Bronze II] Title: 분산처리, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,24 +1,7 @@
 for _ in range(int(input())):
     a, b = map(int, input().split())
     a = a % 10
-    n = 0
     if a == 0:
         print(10)
-        n += 1
-    elif b == 1:
-        print(a)
-        n += 1
-    elif b == 2:
-        print(a ** b % 10)
-        n += 1
     else:
-        for i in range(1, b + 1):
-            if a == a ** (i + 1) % 10:
-                if b % i == 0:
-                    print(a ** i % 10)
-                else:
-                    print(a ** (b % i) % 10)
-                n += 1
-                break
-    if not(n):
-        print(a ** b % 10)
+        print(a ** (b % 4 + 4) % 10)
