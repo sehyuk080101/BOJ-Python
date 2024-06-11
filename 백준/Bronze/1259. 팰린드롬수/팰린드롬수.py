29f2dd6fee880ea5c1de665767634789699debb2
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2024-06-11 16:08:40 +0900

    [Bronze I] Title: 팰린드롬수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -3,7 +3,7 @@ while True:
     n = input()
     if int(n) == 0:
         break
-    for i in range(int(len(n) / 2)):
+    for i in range(len(n) // 2):
         if n[i] != n[-(i + 1)]:
             s += 1
     if s == 0:
