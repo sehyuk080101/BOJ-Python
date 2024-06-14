1a632b774e18fdcd812b23f1cce9c41fd9600631
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2024-06-15 00:24:26 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,6 +1,5 @@
 n = int(input())
 d = []
-r = []
 m = [1]
 x = 0
 for _ in range(int(input())):
@@ -11,11 +10,10 @@ while True:
     for i in d:
         for j in range(len(i)):
             if i[j] in m:
-                m.append(i[j - 1])
-                if i[j - 1] not in r and i[j - 1] != 0:
-                    r.append(i[j - 1])
+                if i[j - 1] not in m:
+                    m.append(i[j - 1])
                 i[j - 1], i[j] = 0, 0
                 y += 1
     if y == x:
         break
-print(len(r))
+print(len(m) - 1)
