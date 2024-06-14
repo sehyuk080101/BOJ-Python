1e83d6d1893a2b1866483ae5d17a1244df9cc3d7
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2024-06-14 15:41:44 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,10 +1,13 @@
 h = []
 for _ in range(9):
     h.append(int(input()))
+r = False
 for i in range(8):
     for j in range(i + 1, 9):
         if sum(h) - (h[i] + h[j]) == 100:
             for k in sorted(h):
                 if k != h[i] and k != h[j]:
                     print(k)
-            exit()
+                    r = True
+    if r:
+        break
