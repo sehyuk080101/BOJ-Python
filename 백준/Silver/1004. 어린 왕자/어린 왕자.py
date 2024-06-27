b753053cf97497db4f98669ced07bfecc0ab4553
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2024-06-27 15:14:26 +0900

    [Silver III] Title: 어린 왕자, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -7,6 +7,6 @@ for _ in range(int(sys.stdin.readline())):
         cx, cy, r = map(int, sys.stdin.readline().split())
         d1 = ((cx - x1) ** 2 + (cy - y1) ** 2) ** (1 / 2)
         d2 = ((cx - x2) ** 2 + (cy - y2) ** 2) ** (1 / 2)
-        if (d1 > r and d2 < r) or (d1 < r and d2 > r):
+        if (d1 > r) ^ (d2 > r):
             escape += 1
     sys.stdout.write(str(escape) + "\n")
