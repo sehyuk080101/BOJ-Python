41323370ee66e5a940ab68dd45de306b7164e9ac
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2024-06-27 14:40:46 +0900

    [Silver III] Title: 어린 왕자, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -1,10 +1,12 @@
-T = int(input())
-for i in range(T):
+import sys
+
+for _ in range(int(sys.stdin.readline())):
     escape = 0
-    x1, y1, x2, y2 = map(int, input().split())
-    n = int(input())
-    for j in range(n):
-        cx, cy, r = map(int, input().split())
-        if (((((cx - x1) ** 2 + (cy - y1) ** 2) ** (1 / 2)) < r) and ((((cx - x2) ** 2 + (cy - y2) ** 2) ** (1 / 2)) > r)) or (((((cx - x1) ** 2 + (cy - y1) ** 2) ** (1 / 2)) > r) and ((((cx - x2) ** 2 + (cy - y2) ** 2) ** (1 / 2)) < r)):
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+    for i in range(int(sys.stdin.readline())):
+        cx, cy, r = map(int, sys.stdin.readline().split())
+        d1 = ((cx - x1) ** 2 + (cy - y1) ** 2) ** (1 / 2)
+        d2 = ((cx - x2) ** 2 + (cy - y2) ** 2) ** (1 / 2)
+        if (d1 > r and d2 < r) or (d1 < r and d2 > r):
             escape += 1
-    print(escape)
+    sys.stdout.write(str(escape) + "\n")
