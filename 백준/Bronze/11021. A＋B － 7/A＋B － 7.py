544c39b25e00d7ef676d28e36ba926b3610d17b2
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2024-06-29 19:43:35 +0900

    [Bronze V] Title: A+B - 7, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -1,4 +1,3 @@
-n = int(input())
-for i in range(1, n + 1):
+for i in range(int(input())):
     a, b = map(int, input().split())
-    print(f"Case #{i}: {a + b}")
+    print("Case #%d: %d" %(i + 1, a + b))
