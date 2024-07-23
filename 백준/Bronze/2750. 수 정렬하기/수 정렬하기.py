28e097491ee582f42cf6fcb609f69c11ea874e6b
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2024-07-23 15:56:07 +0900

    [Bronze II] Title: 수 정렬하기, Time: 232 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,19 +1,14 @@
+n = int(input())
+
 arr = []
 
-for i in range(int(input())):
+for i in range(n):
     arr.append(int(input()))
 
-while True:
-    k = 0
-
-    for i in range(1, len(arr)):
-        if arr[i - 1] > arr[i]:
-            arr[i - 1], arr[i] = arr[i], arr[i - 1]
-        else:
-            k += 1
-
-    if k == len(arr) - 1:
-        break
+for i in range(len(arr)):
+    for j in range(len(arr)):
+        if arr[i] < arr[j]:
+            arr[i], arr[j] = arr[j], arr[i]
 
 for i in arr:
     print(i)
