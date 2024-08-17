f46afb1dc4def8b11aa686dcd1c2a1b30c21e760
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2024-08-18 00:49:42 +0900

    [Bronze I] Title: 최소공배수, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,6 +1,8 @@
 for _ in range(int(input())):
-    A, B = map(int, input().split())
-    for i in range(max(A, B), 0, -1):
-        if A % i == 0 and B % i == 0:
-            print(A * B // i)
-            break
+    a, b = map(int, input().split())
+    c, d = a, b
+    if a < b:
+        a, b = b, a
+    while a % b != 0:
+        a, b = b, a % b
+    print(c * d // b)
