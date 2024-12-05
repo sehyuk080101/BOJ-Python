499ed7903dbed3862d15f800dbb2c484e5a33dfb
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2024-12-05 16:03:09 +0900

    [Silver II] Title: 최대 힙, Time: 276 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,31 @@
+import sys
+
+heap = [0]
+for _ in range(int(sys.stdin.readline())):
+    n = int(sys.stdin.readline())
+    if n == 0:
+        size = len(heap) - 1
+        if size == 0:
+            print(0)
+        else:
+            print(heap[1])
+            last = heap[size]
+            p = 1
+            i = 2
+            while i <= size:
+                if i < size and heap[i] < heap[i + 1]:
+                    i += 1
+                if last >= heap[i]:
+                    break
+                heap[p] = heap[i]
+                p = i
+                i *= 2
+            heap[p] = last
+            heap.pop()
+    else:
+        heap.append(n)
+        i = len(heap) - 1
+        while i != 1 and n > heap[i // 2]:
+            heap[i] = heap[i // 2]
+            i //= 2
+        heap[i] = n
