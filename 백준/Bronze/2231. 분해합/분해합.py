ebadc64c5e2c2ce3c18d4c9d654740f7f074c959
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2025-03-24 20:00:49 +0900

    [Bronze II] Title: 분해합, Time: 1452 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,10 +1,10 @@
-N = int(input())
+n = int(input())
 res = 0
-for i in range(N):
+for i in range(n):
     s = i
     for j in str(i):
         s += int(j)
-    if s == N:
+    if s == n:
         res = i
         break
 print(res)
