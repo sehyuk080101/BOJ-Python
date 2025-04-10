c0c7721513e6c984567fc074bf9df7e8a1005b50
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2025-04-10 14:59:52 +0900

    [Bronze II] Title: 숫자, Time: 100 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.py
+++ 백준/Bronze/10093. 숫자/숫자.py
@@ -1,9 +1,9 @@
-A, B = map(int, input().split())
-a = min(A, B)
-b = max(A, B)
-n = b - a - 1
-if a == b:
+a, b = map(int, input().split())
+n1 = min(a, b)
+n2 = max(a, b)
+n = n2-n1-1
+if n1 == n2 or n1 + 1 == n2:
     n = 0
 print(n)
-for i in range(a + 1, b):
+for i in range(n1 + 1, n2):
     print(i, end = ' ')
