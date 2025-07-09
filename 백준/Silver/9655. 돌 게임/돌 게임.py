f622b32042bf71a6aac278a4c36adac5141e03c5
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2025-07-09 19:00:03 +0900

    Update 돌 게임.py

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,14 +1,4 @@
-n = int(input())
-a = 0
-
-while n > 0:
-    if n % 3 == 0:
-        n -= 3
-    else:
-        n -= 1
-    a += 1
-
-if a % 2 == 0:
+if int(input()) % 2 == 0:
     print("CY")
 else:
     print("SK")
