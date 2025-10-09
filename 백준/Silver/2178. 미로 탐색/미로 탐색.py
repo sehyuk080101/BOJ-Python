57b273b17ede9af19aee647ce0b251c5ec140bbf
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2025-10-10 02:32:16 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,48 +1,36 @@
 from collections import deque
 
-graph = {}
+n, m = map(int, input().split())
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int, input())))
+
+xl = [-1, 1, 0, 0]
+yl = [0, 0, -1, 1]
 
 
-def bfs(start_v, n, m):
-    discovered = []
-    queue = deque([[start_v, 1]])
+def bfs(x, y):
+    queue = deque([(x, y)])
 
     while queue:
-        v = queue.popleft()
-        if v[0] == n * m:
-            return v[1]
+        x, y = queue.popleft()
 
-        if v[0] not in discovered:
-            discovered.append(v[0])
+        for i in range(4):
+            nx = x + xl[i]
+            ny = y + yl[i]
 
-            for w in graph[v[0]]:
-                queue.append([w, v[1] + 1])
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
 
-    return None
+            if graph[nx][ny] == 0:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[n - 1][m - 1]
 
 
-n, m = map(int, input().split())
-l = []
-
-for i in range(n):
-    l.append(list(map(int, input())))
-
-for i in range(n):
-    for j in range(m):
-        a = m * i + j + 1
-        if l[i][j] == 1:
-            graph[a] = []
-            if j > 0:
-                if l[i][j - 1] == 1:
-                    graph[a].append(a - 1)
-            if j < m - 1:
-                if l[i][j + 1] == 1:
-                    graph[a].append(a + 1)
-            if i > 0:
-                if l[i - 1][j] == 1:
-                    graph[a].append(a - m)
-            if i < n - 1:
-                if l[i + 1][j] == 1:
-                    graph[a].append(a + m)
-
-print(bfs(1, n, m))
+print(bfs(0, 0))
