4a461622ecbf091ce99caee39fb3f8111b7333fc
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2025-10-11 18:20:08 +0900

    [Silver III] Title: 주유소, Time: 64 ms, Memory: 42660 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,36 @@
+n = int(input())
+distances = list(map(int, input().split()))
+prices = list(map(int, input().split()))
+li = []
+result = 0
+
+
+def m(l):
+    index = 0
+    value = 1000000001
+
+    for i in range(len(l)):
+        if l[i] < value:
+            value = l[i]
+            index = i
+
+    return index
+
+
+c = prices[:n - 1]
+
+while True:
+    a = m(c)
+    li.append(a)
+    c = c[:a]
+
+    if a == 0:
+        break
+
+for i in range(len(li) - 1, -1, -1):
+    b = li[i - 1]
+    if i == 0:
+        b = n - 1
+    result += prices[li[i]] * sum(distances[li[i]:b])
+
+print(result)
