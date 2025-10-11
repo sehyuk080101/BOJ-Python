1fad1bba979087fed022852f0c8f75af091f9a30
임세혁 <164876765+sehyuk080101@users.noreply.github.com>
2025-10-11 18:33:00 +0900

    [Silver III] Title: 주유소, Time: 108 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -3,36 +3,13 @@ import sys
 n = int(sys.stdin.readline())
 distances = list(map(int, sys.stdin.readline().split()))
 prices = list(map(int, sys.stdin.readline().split()))
-li = []
-result = 0
+result = distances[0] * prices[0]
+m = prices[0]
 
+for i in range(1, n - 1):
+    if prices[i] < m:
+        m = prices[i]
 
-def m(l):
-    index = 0
-    value = 1000000001
-
-    for i in range(len(l)):
-        if l[i] < value:
-            value = l[i]
-            index = i
-
-    return index
-
-
-c = prices[:n - 1]
-
-while True:
-    a = m(c)
-    li.append(a)
-    c = c[:a]
-
-    if a == 0:
-        break
-
-for i in range(len(li) - 1, -1, -1):
-    b = li[i - 1]
-    if i == 0:
-        b = n - 1
-    result += prices[li[i]] * sum(distances[li[i]:b])
+    result += m * distances[i]
 
 print(result)
